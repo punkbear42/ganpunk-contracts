5b6716cb772de64c8ef949e91ac8ac23146716a9
punkbear42 <punkbear@protonmail.com>
2023-03-06 20:56:24 +0100

    remove IQuoter

--- contracts/GanPunk.sol
+++ contracts/GanPunk.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
